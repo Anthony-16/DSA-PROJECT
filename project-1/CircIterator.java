a40ef3727607f82cb7c34cf52570291f443387be
Anthony-16 <Delpezzoanthony@aol.com>
2025-04-22 02:14:09 -0400

    Update CircIterator.java

--- project-1/CircIterator.java
+++ project-1/CircIterator.java
@@ -1,9 +1,9 @@
 import java.util.Iterator;
 
-public class CircIterator implements Iterator<Object> {
+public class CircIterator<E> implements Iterator<E>{
 
-	Node next;
-	Node previous;
+	private Node<E> next;
+	private Node<E> previous;
 	boolean nextCalled = false;
 	
 	
@@ -14,24 +14,53 @@ public class CircIterator implements Iterator<Object> {
 	}
 	
 	
+	public CircIterator(Node<E> current, int size) {
+		Node<E> temp = current;
+		for (int i = 0; i < size - 1; i++) {
+			temp = temp.getNext();
+		}
+		previous = temp;
+		next = current.getNext(); 
+		nextCalled = false;
+	}
+	
+	
 	
 	@Override
 	public boolean hasNext() {
 		// TODO Auto-generated method stub
-		return false;
+		if(previous == null || next == null) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
-	public Node next() {
-		// TODO Auto-generated method stub
-		return null;
+	public E next() {
+		E data = next.getElement();
+		previous = previous.getNext();
+		next = next.getNext();
+		nextCalled = true;
+		return data;
 	}
+	
+	
 	
 	public void remove() {
-		
+		if (!nextCalled) {
+			return;
+		}
+		if (next == previous) {
+		nextCalled = false;
+		next = null;
+		previous = null;
+		}
+		else {
+		previous.setNext(next.getNext());
+		nextCalled = false;
+		}
 	}
 	
 	
-	}
-
+	
 }
