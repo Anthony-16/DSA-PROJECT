d0c68023c8a341f526cc8bfdc9ebb9d347da04d9
Anthony-16 <Delpezzoanthony@aol.com>
2025-04-26 15:13:17 -0400

    Update RelationEdge.java

--- project-3/RelationEdge.java
+++ project-3/RelationEdge.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class RelationEdge {
+    private String relationType;
+    private EntityNode source;
+    private EntityNode destination;
+
+    public RelationEdge(String relationType, EntityNode source, EntityNode destination) {
+        this.relationType = relationType;
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public String getRelationType() {
+        return relationType;
+    }
+
+    public EntityNode getSource() {
+        return source;
+    }
+
+    public EntityNode getDestination() {
+        return destination;
+    }
+
+    @Override
+    public String toString() {
+        return source + " --" + relationType + "--> " + destination;
+    }
 }
