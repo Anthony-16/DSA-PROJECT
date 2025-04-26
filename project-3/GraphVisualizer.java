103ebbfcf5bb8b262d72e56eabe0776a3ba2f921
Anthony-16 <Delpezzoanthony@aol.com>
2025-04-26 15:15:39 -0400

    Update GraphVisualizer.java

--- project-3/GraphVisualizer.java
+++ project-3/GraphVisualizer.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 
 public class GraphVisualizer {
+    public static void display(KnowledgeGraph knowledgeGraph) {
+        System.setProperty("org.graphstream.ui", "swing");
+        Graph graph = new SingleGraph("Knowledge Graph");
+
+        // Add all nodes
+        knowledgeGraph.getEntities().forEach(entity -> {
+            String nodeId = entity.getName();
+            graph.addNode(nodeId).setAttribute("ui.label", nodeId);
+        });
+
+        // Add all edges
+        knowledgeGraph.getRelations().forEach(edges -> {
+            edges.forEach(edge -> {
+                String edgeId = edge.getSource() + "-" + edge.getRelationType() + "-" + edge.getDestination();
+                graph.addEdge(edgeId, edge.getSource().getName(), edge.getDestination().getName(), true)
+                        .setAttribute("ui.label", edge.getRelationType());
+            });
+        });
+
+        graph.display();
+    }
 }
